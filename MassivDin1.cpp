55a867ffce0efc408e63e24484c3b988ca96e14c
Artyom <kolmazeff@yandex.ru>
2025-01-22 17:05:45 +0300

    First commit

---
+++ MassivDin1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int arrSize;
+	int NumberArr;
+	std::cout << "Введите элемент массива: ";
+	std::cin >> arrSize;
+	int* arr = new int[arrSize];
+	for (int ArrElement = 0; ArrElement < arrSize; ArrElement++)
+	{
+		std::cout << "arr [" << ArrElement << "] = ";
+		std::cin >> NumberArr;
+		std::cout << std::endl;
+		arr[ArrElement] = NumberArr;
+	}
+	std::cout << "Введённый массив: ";
+	for (int ArrrElement = 0;ArrrElement < arrSize; ArrrElement++) 
+	{
+		std::cout << arr[ArrrElement]<<' ';
+	}
+	delete[] arr;
+}
+
