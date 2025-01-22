495f0a15c166f6126c680d840132e2bcd4343704
Artyom <kolmazeff@yandex.ru>
2025-01-22 17:10:12 +0300

    First commit MassivDin2

---
+++ MassivDin2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+double* create_array(int SizeArr) 
+{
+    return new double [SizeArr] {};
+}
+
+int main(  )
+{
+    setlocale(LC_ALL, "Ru");
+    int Arrsize;
+    std::cout << "Введите размер массива: ";
+    std::cin >> Arrsize;
+    double* Arr = create_array(Arrsize);
+    std::cout << "Массив: " ;
+    for (int ASize = 0 ;ASize < Arrsize;ASize++ ) 
+    {
+        std::cout << Arr[ASize]<< " ";
+    }
+    delete[] Arr;
+}
+
+
